2591096fa5b62b3a6aa9e8bb7698d918e581ebcc
abr4xas <me@abr4xas.org>
2017-07-14 16:04:02 -0400

    agrega controlador admin user * Controlador para administracion de usuarios * Controlador para publicaciones de usuario * Modificacion de las rutas

--- routes/web.php
+++ routes/web.php
@@ -53,7 +53,7 @@ Route::group(['prefix' => 'admin','middleware' => 'auth'], function () {
     Route::middleware(['role:Admin'])->group(function () {
 
         Route::get('/home', 'HomeController@index')->name('admin.home');
-        Route::resource('users', 'UserController');
+        Route::resource('users', 'AdminUserController');
         Route::resource('roles', 'RoleController');
         Route::resource('posts', 'PostController');
 
